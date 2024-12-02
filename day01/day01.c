22e84d43bd9acf7d0c76067b68d1f04d405c0c88
jmkopper <jay.kopper@gmail.com>
2024-12-01 19:49:30 -0500

    Update day01.c

--- day01/day01.c
+++ day01/day01.c
@@ -1,26 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_LINES 1000
 #define MAX_VAL 100000
 
-int read_input(const char* fn, int* lefts, int* rights) {
+int read_input(const char* fn, int** lefts, int** rights) {
     // read `fn` into the left and right buffers
     // returns the number of elements
     int i = 0;
     int current_val = 0;
     int left = 1;
+    int n = 0;
     FILE* fp = fopen(fn, "r");
+
+    while (1) {
+        char c = fgetc(fp);
+        if (c == '\n') {
+            n++;
+        } else if (c == EOF) {
+            break;
+        }
+    }
+    rewind(fp);
+
+    *lefts = malloc(sizeof(int) * n);
+    *rights = malloc(sizeof(int) * n);
 
     while (1) {
         char c = fgetc(fp);
 
         if (c == ' ' && left == 1) {
             left = 0;
-            lefts[i] = current_val;
+            (*lefts)[i] = current_val;
             current_val = 0;
         } else if (c == '\n') {
-            rights[i] = current_val;
+            (*rights)[i] = current_val;
             current_val = 0;
             left = 1;
             i += 1;
@@ -34,7 +47,7 @@ int read_input(const char* fn, int* lefts, int* rights) {
         }
     }
 
-    return i;
+    return n;
 }
 
 int cmp_ints(const void* a, const void* b) {
@@ -73,13 +86,13 @@ int part2(int* lefts, int* rights, int n) {
 }
 
 int main(int argc, char **argv) {
-    int* lefts = malloc(sizeof(int) * NUM_LINES);
-    int* rights = malloc(sizeof(int) * NUM_LINES);
+    int *lefts;
+    int *rights;
+    int n = read_input(argv[1], &lefts, &rights);
 
-    read_input(argv[1], lefts, rights);
-
-    int p1 = part1(lefts, rights, NUM_LINES);
-    int p2 = part2(lefts, rights, NUM_LINES);
+    int p1 = part1(lefts, rights, n);
+    int p2 = part2(lefts, rights, n);
+    
     printf("Part 1: %d\n", p1);
     printf("Part 2: %d\n", p2);
 }
